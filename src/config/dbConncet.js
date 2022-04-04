dda43aecded41215cfba5c9004ccded7255228a1
Franco <francoramiresdosanto@gmail.com>
2022-04-04 10:31:05 -0300

    Adicionando métodos para adicionar, atualizar e remover livros.

--- src/config/dbConncet.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose"
-
-mongoose.connect(//add MongoDB URI for connection with database);
-
-let db = mongoose.connection
-
-export default db;
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -1,10 +1,13 @@
 import express from "express"
-import livroController from "../controllers/livrosController.js"
+import LivroController from "../controllers/livrosController.js"
 
-const router = express.Router()
+const router = express.Router();
 
 router 
-    .get("/livros", livroController.listarLivros)
+    .get("/livros", LivroController.listarLivros)
+    .get("/livros/:id", LivroController.listarLivroPorId)
+    .post("/livros", LivroController.cadastrarLivro)
+    .put("/livros/:id", LivroController.atualizarLivro)
+    .delete("/livros/:id", LivroController.excluirLivro)
 
-    
 export default router; 
